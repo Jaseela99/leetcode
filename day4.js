979528cd9589751af49c52e17665b1d9b985ddcd
Jaseela99 <jaseelachittur@gmail.com>
2022-06-01 13:43:45 +0530

    added roman

--- day4.js
+++ day4.js
@@ -4,21 +4,33 @@ Input: nums = [1,2,3,4]
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4]. */
 
-let nums = [1, 2, 3, 4];
-var runningSum = function (nums) {
-  //initialized a variable sum
-  let sum = 0;
-  //an empty arrayto have the total sums
-  let runSum = [];
-  //looping over
-  for (let i = 0; i < nums.length; i++) {
-    //for every i sum is increased and is pushed to runSum
-    sum = sum + nums[i];
-    runSum.push(sum);
-  }
-  return runSum;
+
+let nums= [1,2,3,4]
+var runningSum = function(nums) {
+
+
+   /* //initialized a variable sum
+    let sum=0
+    //an empty arrayto have the total sums
+    let runSum =[]
+    //looping over
+    for(let i=0;i<nums.length;i++){
+        //for every i sum is increased and is pushed to runSum
+        sum = sum + nums[i]
+         runSum.push(sum)
+    }
+    return runSum */
+
+
+    ///////////////////////////////////////////////////////////////////
+
+    let sum = 0
+    //for every num ,sum is added up and returned in an array
+    let runSum = nums.map(num=>{
+        console.log(sum,num)
+        return sum = num +sum
+    })
+    return runSum
+
 };
-runningSum(nums);
-
-
-//********************************************************************************* */
+console.log(runningSum(nums))
