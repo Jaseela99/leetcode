86f06ccb154b6605c1fc7b920b379fe32399cf25
Jaseela99 <jaseelachittur@gmail.com>
2022-06-09 23:51:16 +0530

    added todays question

--- week2.js
+++ week2.js
@@ -202,6 +202,7 @@ var removePalindromeSub = function(s) {
   if (s.length===0)
       return 0
       //reversed string
+      //split - to array  reverse-reverse the array join- joins the array elements in to string
  let  reversed = s.split("").reverse().join("")
  //if they are equal it is removed in one step otherwise its 2
  if(s===reversed){
@@ -213,3 +214,36 @@ var removePalindromeSub = function(s) {
 };
 
 console.log(removePalindromeSub(s))
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////day5
+
+/* Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+ */
+let numbers = [2,7,11,15]
+let target=9
+var twoSum = function (numbers,target){
+  //first and last index
+  let p1 = 0
+  let p2 =numbers.length-1
+//while start<=end
+  while(p1<=p2){
+    //if the sum =target then indexes are returned in an array
+    if(numbers[p1]+ numbers[p2]===target){
+      return [p1+1,p2+1]
+    }
+    //if sum>target then index is given in reverse otherwise p1 increased by 1
+    numbers[p1] + numbers[p2] > target ?
+    p2-- : p1++
+  }
+  //if p1>p2  return empty array
+  return []
+
+}
+console.log(twoSum(numbers,target))
+
+
