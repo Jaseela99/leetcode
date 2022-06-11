48746fb5cf31ab0c607676cf7edd0230d341db9c
Jaseela99 <jaseelachittur@gmail.com>
2022-06-11 23:58:58 +0530

    added todays

--- week2.js
+++ week2.js
@@ -291,3 +291,36 @@ console.log(lengthOfLongestSubstring(s))
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////day7
+
+/* You are given an integer array nums and an integer x. In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x. 
+Note that this modifies the array for future operations.
+Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1
+Input: nums = [1,1,4,2,3], x = 5
+Output: 2
+Explanation: The optimal solution is to remove the last two elements to reduce x to zero. */
+
+let nums = [1,1,4,2,3]
+let x=5
+
+var minOperations = function(nums, x) {
+  let target = -x, res = -1, sum = 0, left = 0;
+  nums.forEach(element => {
+      target += element;
+  }); 
+  if(target === 0) return nums.length;
+  if(target < 0) return -1;
+  nums.forEach(function(element, index){
+      sum += element;
+      while(sum > target){
+          sum -= nums[left++];
+      }
+      if(sum === target){
+          res = Math.max(res, index - left + 1);
+      }
+  })
+  if(res === -1) return -1;
+  else return nums.length - res; 
+
+}
+
+console.log(minOperations(nums,x))
