35f053bd1a0b7148e0d42cb40b61c6e122085278
Jaseela99 <jaseelachittur@gmail.com>
2022-06-14 22:46:25 +0530

    added todays question

--- week3.js
+++ week3.js
@@ -52,7 +52,7 @@ Explanation: The triangle looks like:
 4 1 8 3
 The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above). */
 
-let triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+/* let triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
 
 var minimumTotal = function(triangle) {
     //reverses the triangle array=> dynamic programming we have choose the minimum from the two adjacent indexes =>behaves similar to binary tree
@@ -67,4 +67,68 @@ var minimumTotal = function(triangle) {
 })
 return newtri[newtri.length-1] //returns last element of arr or use pop method
  }
- console.log(minimumTotal(triangle))
+ console.log(minimumTotal(triangle)) */
+
+/////////////////////////////////////////////////////////////////
+
+//////////day3
+
+/* Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+In one step, you can delete exactly one character in either string. 
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".*/
+
+let word1 ="sea"
+let word2 ="eat"
+
+var minDistance = function(word1, word2) {
+  if (word1.length < word2.length) return minDistance(word2, word1);
+  
+  if (word2.length == 0) return word1.length;
+  
+  let m = word1.length, n = word2.length;
+  
+  let dp = new Array(n + 1);
+  
+  for (let i = 0; i <= m; i++) {  
+      let prev = 0;
+      for (let j = 0; j <= n; j++) {   
+          let temp = dp[j];
+          if (i == 0 || j == 0) {
+              dp[j] = i + j;
+          } else if (word1.charAt(i-1) === word2.charAt(j-1)) {
+              
+              dp[j] = prev;
+          } else {
+              
+              dp[j] = 1 + Math.min(dp[j], dp[j-1]);
+          }
+          
+          prev = temp;
+      }
+  }
+  
+  return dp[n];
+  
+
+
+/////****************************************************** */
+
+/* if (word1.length < word2.length) return minDistance(word2, word1);
+    
+const dp = Array(word1.length + 1);
+   for (let i = 0; i < word1.length + 1; i++) {
+       dp[i] = i;
+   }
+   for (let j = 1; j < word2.length + 1; j++) {
+       let last = dp[0];
+       dp[0] = j;
+       for (let i = 1; i < word1.length + 1; i++) {
+           const current = dp[i];
+           dp[i] = word1[i-1] === word2[j-1] ? last : (Math.min(dp[i], dp[i-1]) + 1);
+           last = current;
+       }
+   }
+   return dp[word1.length]; */
+  }
