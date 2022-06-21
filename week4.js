9f1531660c25c22390467917e0af26c45ad1eb7b
Jaseela99 <jaseelachittur@gmail.com>
2022-06-21 13:05:51 +0530

    added todays question

--- week4.js
+++ week4.js
@@ -25,3 +25,30 @@ var minimumLengthEncoding = function(words) {
      return Array.from(set).join().length + 1  
  };
  console.log(minimumLengthEncoding(words))
+ //////////////////////////////////////////////////////////////////////////////
+ /////////day2
+
+/* You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
+You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
+While moving from building i to building i+1 (0-indexed),
+If the current building's height is greater than or equal to the next building's height, you do not need a ladder or bricks.
+If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
+Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally. */
+let heights = [4,2,7,6,9,14,12]  
+let  bricks = 5
+let  ladders = 1
+var furthestBuilding = function(heights, bricks, ladders) {
+    let len = heights.length - 1,
+    pq = new MinPriorityQueue({priority: x => x})
+for (let i = 0; i < len; i++) {
+    let diff = heights[i+1] - heights[i]
+    if (diff > 0) {
+        if (ladders > 0) pq.enqueue(diff), ladders--
+        else if (pq.front() && diff > pq.front().element)
+            pq.enqueue(diff), bricks -= pq.dequeue().element
+        else bricks -= diff
+        if (bricks < 0) return i
+    }
+}
+return len
+};
