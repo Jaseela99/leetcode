5383f718792febb0ab827732cf5f94260b41681d
Jaseela99 <jaseelachittur@gmail.com>
2022-06-23 11:50:20 +0530

    added todays

--- week4.js
+++ week4.js
@@ -69,3 +69,29 @@ var findKthLargest = function(nums, k) {
     return nums[nums.length-k]
   };
   console.log(findKthLargest(nums,k))
+  ///////////////////////////////////////////////
+  /////////day4
+  /* There are n different online courses numbered from 1 to n.
+ You are given an array courses where courses[i] = [durationi, lastDayi] 
+ indicate that the ith course should be taken continuously for durationi days and must be finished before or on lastDayi.
+You will start on the 1st day and you cannot take two or more courses simultaneously.
+Return the maximum number of courses that you can take.
+Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+Output: 3
+Explanation: 
+There are totally 4 courses, but you can take 3 courses at most:
+First, take the 1st course, it costs 100 days so you will finish it on the 100th day, and ready to take the next course on the 101st day.
+Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day. 
+Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day. 
+The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the closed date. */
+let courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+var scheduleCourse = function(courses) {
+    courses.sort((a,b) => a[1] - b[1])
+    let total = 0, pq = new MaxPriorityQueue({priority: x => x})
+    for (let [dur, end] of courses)
+        if (dur + total <= end)
+            total += dur, pq.enqueue(dur)
+        else if (pq.front() && pq.front().element > dur)
+            total += dur - pq.dequeue().element, pq.enqueue(dur)
+    return pq.size()  
+};
