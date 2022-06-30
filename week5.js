e35ccdd97d69fac9d1a2ce26b4df6514ef31c081
Jaseela99 <jaseelachittur@gmail.com>
2022-06-30 11:54:31 +0530

    added todays

--- week5.js
+++ week5.js
@@ -65,3 +65,20 @@ var reconstructQueue = function(people) {
     })
     return res 
 };
+///////////////////////////////////
+/////////day5
+/* Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+In one move, you can increment or decrement an element of the array by 1.
+Test cases are designed so that the answer will fit in a 32-bit integer.
+Input: nums = [1,2,3]
+Output: 2
+Explanation:
+Only two moves are needed (remember each move increments or decrements one element):
+[1,2,3]  =>  [2,2,3]  =>  [2,2,2] */
+
+    var minMoves2 = function(nums) {
+        nums.sort((a,b) => a - b)
+         let ans = 0, median = nums[~~(nums.length / 2)]
+         for (let i = 0; i < nums.length; i++) ans += Math.abs(median - nums[i])
+         return ans  
+     };
