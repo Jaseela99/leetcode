e94ff28ee25b0758bc94633d72d2931f339b6638
Jaseela99 <jaseelachittur@gmail.com>
2022-07-02 13:34:31 +0530

    added todays

--- week5.js
+++ week5.js
@@ -109,3 +109,21 @@ var maximumUnits = function(boxTypes, truckSize) {
        }
        return totalUnits 
    };
+   /////////////////////////////////////////
+   /////day 6
+   /* You are given a rectangular cake of size h x w and two arrays of integers horizontalCuts and verticalCuts where:
+horizontalCuts[i] is the distance from the top of the rectangular cake to the ith horizontal cut and similarly, and
+verticalCuts[j] is the distance from the left of the rectangular cake to the jth vertical cut.
+Return the maximum area of a piece of cake after you cut at each horizontal and vertical position provided in the arrays horizontalCuts and verticalCuts. Since the answer can be a large number, return this modulo 109 + 7. */
+
+var maxArea = function(h, w, horizontalCuts, verticalCuts) {
+    horizontalCuts.sort((a,b) => a - b)
+     verticalCuts.sort((a,b) => a - b)
+     let maxh = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length-1]),
+         maxv = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length-1])
+     for (let i = 1; i < horizontalCuts.length; i++)
+         maxh = Math.max(maxh, horizontalCuts[i] - horizontalCuts[i-1])
+     for (let i = 1; i < verticalCuts.length; i++)
+         maxv = Math.max(maxv, verticalCuts[i] - verticalCuts[i-1])
+     return BigInt(maxh) * BigInt(maxv) % 1000000007n 
+ };
