1588fb8d5819c566c1bafb588f6e5422b9d4d164
Jaseela99 <jaseelachittur@gmail.com>
2022-07-03 10:20:55 +0530

    added todays

--- week5.js
+++ week5.js
@@ -127,3 +127,23 @@ var maxArea = function(h, w, horizontalCuts, verticalCuts) {
          maxv = Math.max(maxv, verticalCuts[i] - verticalCuts[i-1])
      return BigInt(maxh) * BigInt(maxv) % 1000000007n 
  };
+ /////////////////////////////////////////////
+ //day7
+/*  A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
+For example, [1, 7, 4, 9, 2, 5] is a wiggle sequence because the differences (6, -3, 5, -7, 3) alternate between positive and negative.
+In contrast, [1, 4, 7, 2, 5] and [1, 7, 4, 5, 5] are not wiggle sequences. The first is not because its first two differences are positive, and the second is not because its last difference is zero.
+A subsequence is obtained by deleting some elements (possibly zero) from the original sequence, leaving the remaining elements in their original order.
+Given an integer array nums, return the length of the longest wiggle subsequence of nums.
+Input: nums = [1,7,4,9,2,5]
+Output: 6
+Explanation: The entire sequence is a wiggle sequence with differences (6, -3, 5, -7, 3). */
+
+var wiggleMaxLength = function(nums) {
+    let len = nums.length, i = 1
+     while (nums[i] === nums[i-1]) i++
+     let up = nums[i-1] > nums[i], ans = 1
+     for (; i < len; i++)
+         if ((up && nums[i] < nums[i-1]) || (!up && nums[i] > nums[i-1]))
+             up = !up, ans++
+     return ans 
+ };
