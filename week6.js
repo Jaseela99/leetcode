040538b023e7951ecdb6f6aaf879f4b3e9aec14b
Jaseela99 <jaseelachittur@gmail.com>
2022-07-07 11:31:26 +0530

    added todays

--- week6.js
+++ week6.js
@@ -75,3 +75,76 @@ Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1. */
      return fib(n-1)+fib(n-2) 
      }
  };
+ console.log(fib(4))
+///////////////////////////////////////////////////////
+
+/////primenumbers
+/*function isPrime(num) {
+    for ( var i = 2; i < num; i++ ) {
+        if ( num % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+ function display(n) {
+    var arr = [2];
+    for ( var i = 3; i < n; i+=2 ) {
+        if ( isPrime(i) ) {
+            arr.push(i);
+        }
+    }
+    console.log(arr); // use arr result on your own
+}
+display(160) */
+
+var enterNumber = function(n){
+for(let i=2; i<=n ;i++){
+        let isPrime = true;
+        for(let j=2; j<i; j++){
+            if(i%j === 0 && i !== j){
+                isPrime = false;
+            }
+        }
+        if(isPrime === true){
+            console.log(i);
+        }
+    }
+}
+enterNumber(101)
+
+
+let arr=[1,2,3,4,5]
+
+const odds= arr.filter(n=>n%2)
+const evens = arr.filter(n=>n%2==0)
+console.log(evens)
+console.log(odds)
+
+///////////////////////////////////////////////////
+///////day4
+
+/* Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings such that:
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+Note: a + b is the concatenation of strings a and b.
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true */
+
+var isInterleave = function(s1, s2, s3) {
+    let n = s1.length + 2, m = s2.length + 2
+    if (n + m - 4 !== s3.length) return false
+    let dp = new Uint8Array(m)
+    dp[1] = 1
+    for (let i = 1; i < n; i++)
+        for (let j = 1; j < m; j++) {
+            let up = dp[j] && s1[i-2] === s3[j+i-3],
+                left = dp[j-1] && s2[j-2] === s3[j+i-3]
+            dp[j] = up || left
+        }
+    return dp[m-1]   
+};
