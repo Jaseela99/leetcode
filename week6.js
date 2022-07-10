22aa705872ff8e7edb1ed77f5f3b92130e46c34e
Jaseela99 <jaseelachittur@gmail.com>
2022-07-10 07:25:05 +0530

    added todays

--- week6.js
+++ week6.js
@@ -244,3 +244,23 @@ var maxResult = function(nums, k) {
     }
     return nums[0] 
 };
+
+//////////////////////////////////////////////
+////day7
+
+/* You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15. */
+
+var minCostClimbingStairs = function(cost) {
+    for (let i = cost.length - 3; ~i; i--)
+      cost[i] += Math.min(cost[i+1], cost[i+2])
+  return Math.min(cost[0], cost[1])
+};
