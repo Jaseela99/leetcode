2b0ae74d88244b61c28313db9a7740d7762ba90a
Jaseela99 <jaseelachittur@gmail.com>
2022-07-12 17:36:23 +0530

    added todays

--- week7.js
+++ week7.js
@@ -16,3 +16,36 @@ var rightSideView = function(root) {
       pre(node.right, h+1);
   } 
 };
+//////////////////////////////////
+/* You are given an integer array matchsticks where matchsticks[i] is the length of the ith matchstick. 
+You want to use all the matchsticks to make one square.
+ You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.
+Return true if you can make this square and false otherwise. */
+
+var makesquare = function(matchsticks) {
+  let n = matchsticks.length, side = matchsticks.reduce((a,c) => a + c) / 4
+    matchsticks.sort((a,b) => b - a)
+    if (side !== ~~side || matchsticks[0] > side)
+        return false
+    const btrack = (i, space, done) => {
+        if (done === 3)
+            return true
+        for (; i < n; i++) {
+            let num = matchsticks[i], res
+            if (num > space)
+                continue
+            matchsticks[i] = side + 1
+            if (num === space)
+                res = btrack(1, side, done+1)
+            else
+                res = btrack(i+1, space-num, done)
+            if (res)
+                return true
+            matchsticks[i] = num
+            while (matchsticks[i+1] === num)
+                i++
+        }
+        return false
+    }
+    return btrack(0, side, 0)  
+};
