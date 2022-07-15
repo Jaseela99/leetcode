44ee41b66e9f089c86632498980377d18af9e997
Jaseela99 <jaseelachittur@gmail.com>
2022-07-15 17:17:59 +0530

    added todays

--- week7.js
+++ week7.js
@@ -90,3 +90,26 @@ var splitTree = function(preorder, M, pix, ileft, iright) {
         root.right = splitTree(preorder, M, pix+imid-ileft+1, imid+1, iright)
     return root 
 };
+
+/////////////////////////////
+
+/* You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid. If there is no island, return 0. 
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+*/
+
+var maxAreaOfIsland = function(grid) {
+    let ans = 0, n = grid.length, m = grid[0].length
+      const trav = (i, j) => {
+          if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0
+          grid[i][j] = 0
+          return 1 + trav(i-1, j) + trav(i, j-1) + trav(i+1, j) + trav(i, j+1)
+      }
+      for (let i = 0; i < n; i++) 
+          for (let j = 0; j < m; j++)
+              if (grid[i][j]) ans = Math.max(ans, trav(i, j))
+      return ans  
+  };
