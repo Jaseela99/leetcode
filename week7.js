a85f0888cb0d577907519a80862665e31d03d09c
Jaseela99 <jaseelachittur@gmail.com>
2022-07-16 12:10:11 +0530

    added todays

--- week7.js
+++ week7.js
@@ -113,3 +113,31 @@ var maxAreaOfIsland = function(grid) {
               if (grid[i][j]) ans = Math.max(ans, trav(i, j))
       return ans  
   };
+
+
+  ////////////////////////////////////////////
+/*   There is an m x n grid with a ball. The ball is initially at the position [startRow, startColumn]. You are allowed to move the ball to one of the four adjacent cells in the grid (possibly out of the grid crossing the grid boundary). You can apply at most maxMove moves to the ball.
+Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move the ball out of the grid boundary. Since the answer can be very large, return it modulo 109 + 7.
+Input: m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0
+Output: 6 */
+
+var findPaths = function(m, n, maxMove, startRow, startColumn) {
+    if (!maxMove) return 0
+        let dpCurr = Array.from({length: m+2}, () => new Uint32Array(n+2)),
+            dpLast = Array.from({length: m+2}, () => new Uint32Array(n+2))
+        for (let i = 1; i <= m; i++)
+            dpCurr[i][1]++, dpCurr[i][n]++
+        for (let j = 1; j <= n; j++)
+            dpCurr[1][j]++, dpCurr[m][j]++
+        let ans = dpCurr[startRow+1][startColumn+1]
+        for (let d = 1; d < maxMove; d++) {
+            [dpCurr, dpLast] = [dpLast, dpCurr]
+            for (let i = 1; i <= m; i++)
+                for (let j = 1; j <= n; j++)
+                    dpCurr[i][j] = (dpLast[i-1][j] + dpLast[i+1][j] + dpLast[i][j-1] + dpLast[i][j+1]) % 1000000007
+            ans = (ans + dpCurr[startRow+1][startColumn+1]) % 1000000007
+        }
+        return ans    
+    };
+
+ 
