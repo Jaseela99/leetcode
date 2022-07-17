83fbfdf1814f91bae763d46a1beef0e4b8b412c8
Jaseela99 <jaseelachittur@gmail.com>
2022-07-17 11:03:05 +0530

    added todays

--- week7.js
+++ week7.js
@@ -140,4 +140,26 @@ var findPaths = function(m, n, maxMove, startRow, startColumn) {
         return ans    
     };
 
+    //////////////////////////////////////
+  /*   For an integer array nums, an inverse pair is a pair of integers [i, j] where 0 <= i < j < nums.length and nums[i] > nums[j].
+Given two integers n and k, return the number of different arrays consist of numbers from 1 to n such that there are exactly k inverse pairs. Since the answer can be huge, return it modulo 109 + 7.
+Input: n = 3, k = 0
+Output: 1
+Explanation: Only the array [1,2,3] which consists of numbers from 1 to 3 has exactly 0 inverse pairs. */
+
+var kInversePairs = function(n, k) {
+    let mod = 1000000007;
+          if (k > n*(n-1)/2 || k < 0) return 0;
+          let dp = new Array(n+1).fill(0).map(() => new Array(k+1).fill(0));
+          for (let i = 1; i <= n; i++) {
+              dp[i][0] = 1;
+              if (i + 1 <= n) dp[i + 1][0] = 1;
+              for (let j = 1; j <= Math.min(k, i*(i-1)/2); j++) {
+                  dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                  if (j >= i) dp[i][j] -= dp[i-1][j-i];
+                  dp[i][j] = (dp[i][j]+mod) % mod;
+              }
+          }
+          return dp[n][k];   
+  };
  
