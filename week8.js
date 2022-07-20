c4312c05dcc2583d0cf1e569309bfa203bb632a8
Jaseela99 <jaseelachittur@gmail.com>
2022-07-20 14:38:30 +0530

    added todays

--- week8.js
+++ week8.js
@@ -20,3 +20,34 @@ var generate = function(numRows) {
     }
   return pascal
 };
+
+///////////////////////////////////////
+/* Given a string s and an array of strings words, return the number of words[i] that is a subsequence of s.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+ 
+
+Example 1:
+
+Input: s = "abcde", words = ["a","bb","acd","ace"]
+Output: 3
+Explanation: There are three strings in words that are a subsequence of s: "a", "acd", "ace". */
+var numMatchingSubseq = function(s, words) {
+  let count = 0
+    for(let w of words){
+      if(findSubsequence(s, w)) count++
+    }
+    return count
+  };
+    
+  function findSubsequence(s, w){
+    let idx = -1
+    for(const c of w){
+      const found = s.indexOf(c, idx+1)
+      if(found == -1) return false
+      idx = found
+    }
+    return true    
+  };
