7f716c16615433e41aab67209b100e1d788e654f
Jaseela99 <jaseelachittur@gmail.com>
2022-07-24 11:59:40 +0530

    added todays

--- week8.js
+++ week8.js
@@ -139,3 +139,41 @@ var countSmaller = function(nums) {
   }
   return counts;
 };
+
+////////////////////////////////////////
+/* Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true */
+var searchMatrix = function(matrix, target) {
+  if(matrix.length === 0){
+      return false;
+  }
+  //start with top-right element
+  var i = 0;
+  var j = matrix[0].length - 1;
+  
+  //loop till row and column number are within bounds
+  while(i <= matrix.length - 1 && j >= 0){
+      
+      if(matrix[i][j] > target){
+          //current element is greater than target
+          //means this row might have the target element
+          //change column
+          j--;
+      }else if(matrix[i][j] === target){
+          //element found
+          return true;
+      }else if(matrix[i][j] < target){
+          //current element is lesser than target
+          //means this column might have the target element
+          //change rows
+          i++;
+      }
+  }
+  return false;
+
+};
