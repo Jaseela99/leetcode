648a7245466b0141287955b7c95ddb521fde1e64
Jaseela99 <jaseelachittur@gmail.com>
2022-07-25 22:51:17 +0530

    added todays

--- week8.js
+++ week8.js
@@ -177,3 +177,22 @@ var searchMatrix = function(matrix, target) {
   return false;
 
 };
+//////////////////////////////////////////
+/* Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4] */
+var searchRange = function(N, T) {
+  const find = (target, arr, left=0, right=arr.length) => {
+      while (left <= right) {
+          let mid = left + right >> 1
+          if (arr[mid] < target) left = mid + 1
+          else right = mid - 1
+      }
+      return left
+  } 
+  let Tleft = find(T, N)
+  if (N[Tleft] !== T) return [-1,-1]
+  return [Tleft, find(T+1, N, Tleft) - 1] 
+};
