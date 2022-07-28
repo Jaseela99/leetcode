95071431ed443b00d736b6007f3acdf868079cb6
Jaseela99 <jaseelachittur@gmail.com>
2022-07-28 12:19:21 +0530

    added todays

--- week9.js
+++ week9.js
@@ -2,7 +2,7 @@
 The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
 The "linked list" should be in the same order as a pre-order traversal of the binary tree. */
 
-var flatten = function(root) {
+/* var flatten = function(root) {
     let head = null
      const revPreOrder = node => {
          if (node.right) revPreOrder(node.right)
@@ -10,4 +10,17 @@ var flatten = function(root) {
          node.left = null, node.right = head, head = node
      }
      if (root) revPreOrder(root) 
- };
+ }; */
+ 
+ /* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once. */
+
+var isAnagram = function(s, t) {
+    let sortedS = s.split('').sort().join('')
+      let sortedT = t.split('').sort().join('')
+      if (sortedS === sortedT) {
+          return true
+      } else {
+          return false
+      }   
+  };
