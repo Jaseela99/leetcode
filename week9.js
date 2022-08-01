a7222cabee199bad08d08f32811f26c79a9358b3
Jaseela99 <jaseelachittur@gmail.com>
2022-08-01 15:54:39 +0530

    added todays

--- week9.js
+++ week9.js
@@ -187,3 +187,20 @@ NumArray.prototype.sumRange = function (left, right) {
     return sum;
 
 };
+
+///////////////////////////////////////////////////
+/* There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+The test cases are generated so that the answer will be less than or equal to 2 * 109. */
+
+var uniquePaths = function(m, n) {
+    const dp = new Array(n + 1).fill(1);
+    
+    for(let row = m - 1; row > 0; row--){
+        for(let col = n - 1; col > 0; col--){
+            dp[col] = dp[col] + dp[col + 1];
+        }
+    }
+    return dp[1];
+};
+
