81321ee85c03921f7c0cde1775dd100b710c1107
Jaseela99 <jaseelachittur@gmail.com>
2022-08-02 21:50:28 +0530

    added todays

--- week9.js
+++ week9.js
@@ -204,3 +204,42 @@ var uniquePaths = function(m, n) {
     return dp[1];
 };
 
+/////////////////
+/* Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+You must find a solution with a memory complexity better than O(n2).
+Example 1:
+Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13 */
+
+var kthSmallest = function(matrix, k) {
+    const n = matrix.length
+    
+    function getCount(target) {
+        let count = 0;
+        
+        for(let i = 0; i < n; i++) {
+            let currCount = 0;
+            
+            for(let j = 0; j < n; j++) {
+                if(matrix[i][j] <= target) currCount++;
+                else break;
+            }
+            count += currCount;
+            if(!currCount) break;
+        }
+        return count;
+    }
+    
+    let start = matrix[0][0], end = matrix[n-1][n-1];
+    
+    while(start < end) {
+        const mid = Math.floor((end-start)/2) + start;
+        const count = getCount(mid);
+        if(count < k) start = mid+1;
+        else end = mid;
+    }
+    return end;
+};
+
