44fd747f8c3aa86f8828bcdd061e97aeefa7ed6a
Jaseela99 <jaseelachittur@gmail.com>
2022-08-03 23:39:20 +0530

    added todays

--- week9.js
+++ week9.js
@@ -242,4 +242,55 @@ var kthSmallest = function(matrix, k) {
     }
     return end;
 };
+///////////////////////////////
+/* You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
 
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+Implement the MyCalendar class:
+
+MyCalendar() Initializes the calendar object.
+boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+
+ */
+
+
+var MyCalendar = function() {
+    this.events = [];
+  };
+  
+  
+  
+  MyCalendar.prototype.book = function(start, end) {
+    this.events.push([start,end])
+    const sorted  = this.events.slice().sort((a,b)=> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+    for(let i=1; i < sorted.length; i++){
+      const prev = sorted[i-1]
+      const curr = sorted[i]
+      if(prev[1] > curr[0]){
+        this.events.pop()
+        return false
+      }
+    }
+    return true
+  };
+  ;
+  
+  
