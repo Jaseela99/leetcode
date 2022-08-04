42a4171ffa28f52b8dd1e87eaf6e0944257060e7
Jaseela99 <jaseelachittur@gmail.com>
2022-08-04 19:47:10 +0530

    added todays

--- week9.js
+++ week9.js
@@ -291,6 +291,30 @@ var MyCalendar = function() {
     }
     return true
   };
-  ;
+  
+
+  /////////////////////////////////////
+  /* There is a special square room with mirrors on each of the four walls. Except for the southwest corner, there are receptors on each of the remaining corners, numbered 0, 1, and 2.
+
+The square room has walls of length p and a laser ray from the southwest corner first meets the east wall at a distance q from the 0th receptor.
+
+Given the two integers p and q, return the number of the receptor that the ray meets first.
+
+The test cases are guaranteed so that the ray will meet a receptor eventually. */
+
+var mirrorReflection = function(p, q) {
+    while (p%2==0 && q%2==0) {
+		p = p / 2
+		q = q / 2
+	}
+	
+	if (p%2 == 0)
+		return 2
+	
+	if (q%2 == 0)
+		return 0
+	
+	return 1
+};
   
   
