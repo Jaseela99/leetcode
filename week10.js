94b9168bdbf381e0d54208225e88f96403a694ea
Jaseela99 <jaseelachittur@gmail.com>
2022-08-07 23:09:13 +0530

    added todays

--- week10.js
+++ week10.js
@@ -28,3 +28,46 @@ var poorPigs = function(buckets, minutesToDie, minutesToTest) {
  
  return Math.ceil(Math.log(buckets) / Math.log(n));
 };
+
+/////////////////////////////////////////////////////////
+/* Given an integer n, your task is to count how many strings of length n can be formed under the following rules:
+
+Each character is a lower case vowel ('a', 'e', 'i', 'o', 'u')
+Each vowel 'a' may only be followed by an 'e'.
+Each vowel 'e' may only be followed by an 'a' or an 'i'.
+Each vowel 'i' may not be followed by another 'i'.
+Each vowel 'o' may only be followed by an 'i' or a 'u'.
+Each vowel 'u' may only be followed by an 'a'.
+Since the answer may be too large, return it modulo 10^9 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: 5
+Explanation: All possible strings are: "a", "e", "i" , "o" and "u". */
+
+var countVowelPermutation = function(n) {
+    const modVal = (1e9 + 7);
+  let a = 1;
+  let e = 1;
+  let i = 1;
+  let o = 1;
+  let u = 1;
+
+  for (let x = 1; x < n; x++) {
+     let tempA = e % modVal;
+     let tempE = (a + i) % modVal;
+     let tempI = (a + e + o + u) % modVal;
+     let tempO = (i + u) % modVal;
+     let tempU = a % modVal;
+     a = tempA;
+     e = tempE;
+     i = tempI;
+     o = tempO;
+     u = tempU;
+  }
+
+  return (a + e + i + o + u) % modVal;
+};
